213baca3a2a431262b446146dfc2c32f45a16fd8
Martin <mgs5727@psu.edu>
2026-04-19 23:03:54 -0400

    Switch to standalone MongoDB on PSU server

--- nodeTest.js
+++ nodeTest.js
@@ -1,7 +1,25 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://martinshestani_db_user:Organics888.@cluster0.nwefo1f.mongodb.net/ist256?appName=Cluster0');
+mongoose.connect('mongodb://team4:team4@130.203.136.203:27017/team4DB');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', () => console.log('MongoDB connected!'));
+db.once('open', async () => {
+  console.log('MongoDB connected!');
+
+  await db.createCollection('shopper');
+  await db.createCollection('products');
+  await db.createCollection('cart');
+  await db.createCollection('shipping');
+  await db.createCollection('returns');
+
+  console.log('Collections created!');
+
+  const result = await db.collection('cart').insertOne({
+    productName: 'Test Product',
+    quantity: 2,
+    price: 49.99
+  });
+
+  console.log('Data saved to MongoDB:', result);
+});
