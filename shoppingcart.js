0606596203ff41981304ddee19015228c24c27fb
yusufbaksh <yib5091@psu.edu>
2026-04-26 15:26:26 -0400

    Change API URL to localhost for development

--- shoppingcart.js
+++ shoppingcart.js
@@ -7,7 +7,7 @@ Supports Create, Read, Update, and Delete with MongoDB.
 
 $(document).ready(function () {
 
-    const API_URL = "http://130.203.136.203:3004/cart";
+    const API_URL = "http://localhost:3004";
     let cart = [];
 
     loadCartFromAPI();
